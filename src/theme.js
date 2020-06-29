65b101f80a6017d28864c45e7e8a40389b73664f
Erik Båvenstrand <erik.bavenstrands@gmail.com>
2020-06-29 11:11:29 +0200

    Added site

--- src/theme.js
+++
@@ -1,8 +0,0 @@
-export const theme = {
-    lightBorder: '#ebebeb',
-    primaryDark: '#0D0C1D',
-    primaryLight: '#FFFFFFFF',
-    primaryGrey: '#EEEEEE',
-    primaryHover: '#343078',
-    mobile: '576px',
-  }
